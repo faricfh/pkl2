9087d9607b83afc07e9f8324438c96053795177c
faricfh <ifake@gmail.com>
2019-06-22 16:43:21 +0700

    add template

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/feature', function () {
+    return view('feature');
+});
+
+Route::get('/price', function () {
+    return view('price');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/single-blog', function () {
+    return view('single-blog');
+});
+
+Route::get('/element', function () {
+    return view('element');
+});
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', function () {
+    return view('backend');
+});
